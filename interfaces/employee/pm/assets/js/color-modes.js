fbea8e7f60cd7e560b0989de2778cd495619bd0d
GreenPeter11 <firstgreen71@gmail.com>
2024-08-06 09:41:58 +0200

    modes.js

--- interfaces/employee/pm/assets/js/color-modes.js
+++ interfaces/employee/pm/assets/js/color-modes.js
@@ -3,6 +3,36 @@
  * Copyright 2011-2024 The Bootstrap Authors
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
+$(document).ready(function() {
+  $('#supplier_id').change(function() {
+      var supplierId = $(this).val();
+      
+      if (supplierId) {
+          $.ajax({
+              url: 'get_products.php',
+              type: 'GET',
+              data: { supplier_id: supplierId },
+              dataType: 'json',
+              success: function(response) {
+                  if (response.status === 'success') {
+                      $('#product_id').empty();
+                      $.each(response.products, function(index, product) {
+                          $('#product_id').append('<option value="' + product.value + '">' + product.text + '</option>');
+                      });
+                  } else {
+                      alert('Error fetching products: ' + response.message);
+                  }
+              },
+              error: function(xhr, status, error) {
+                  console.error('AJAX error:', error);
+                  alert('An error occurred while fetching products.');
+              }
+          });
+      } else {
+          $('#product_id').empty().append('<option value="">Select a product</option>');
+      }
+  });
+});
 
 (() => {
   'use strict'
